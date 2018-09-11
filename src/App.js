ed69d391d9ae9ead47caacfa83311129cac1cf20
David Chellapah <davidc94@btinternet.com>
2018-09-11 11:54:01 +0100

    docs(readme): updated readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import Dashboard from './components/pages/Dashboard';
 
 export default class App extends Component {
