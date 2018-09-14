e05778c4d7e972f7dd0b70cdfa065e3cf48450d1
Tamas Jonas <jonastamas2@gmail.com>
2018-09-14 14:57:28 +0100

    feat(choose-bank-page): added choose bank page (#26)

--- src/components/pages/ChooseBank/index.js
+++ src/components/pages/ChooseBank/index.js
@@ -1,16 +1,60 @@
 import React, { PureComponent } from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, Image, FlatList } from 'react-native';
+import StyledButton from '../../atoms/StyledButton';
+import StyledText from '../../atoms/StyledText';
 
 export default class ChooseBank extends PureComponent {
+  state = {
+    data: [{
+      bankName: 'Barclays',
+      logoImage: require('../../../assets/images/banklogos/barclayslogo.jpg')
+    }, {
+      bankName: 'LloydsBank',
+      logoImage: require('../../../assets/images/banklogos/lloydslogo.jpg')
+    }, {
+      bankName: 'HSBC',
+      logoImage: require('../../../assets/images/banklogos/hsbclogo.jpg')
+    }],
+    selectedBank: false
+  }
+
+  selectBank = (selectedBank) => this.setState({ selectedBank });
+
+  renderListItem = ({ item }) => {
+    const bdColor = item.bankName === this.state.selectedBank ? '#000' : '#ccc';
+    return (
+      <TouchableOpacity onPress={() => this.selectBank(item.bankName)}
+        style={{ flexDirection: 'row', padding: 20, marginTop: 10, marginHorizontal: 10, borderColor: bdColor, borderWidth: 1 }}>
+        <View style={{ width: 50, height: 50, margin: 5, flex: 1 }}>
+          <Image style={{ resizeMode: 'contain' }} source={item.logoImage} />
+        </View>
+        <StyledText style={{ flex: 4 }}>
+          {item.bankName}
+        </StyledText>
+      </TouchableOpacity>
+    )
+  }
   render() {
     return (
       <View>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Consent')}>
-          <Text>
-            ChooseBank
-          </Text>
-        </TouchableOpacity>
-      </View>
+        <StyledText style={{ marginVertical: 30 }}>
+          Select your bank that your day to day spending account is with.
+        </StyledText>
+        <FlatList
+          style={{ marginBottom: 50 }}
+          data={this.state.data}
+          extraData={this.state}
+          renderItem={this.renderListItem}
+        />
+        <View style={{ paddingHorizontal: 80 }}>
+          <StyledButton
+            disabled={!this.state.selectedBank}
+            onPress={() => this.props.navigation.navigate('Consent')}
+          >
+            Connect to bank
+          </StyledButton>
+        </View>
+      </View >
     );
   }
 }
