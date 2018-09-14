8256245bfa3e130b859b31f8a78f6a3a2d8ab3b4
sanjeevbaurai <sanjeev.baurai@gmail.com>
2018-09-14 15:59:40 +0100

    feat(select-account): remove inline styling

--- src/components/pages/ChooseAccount/index.js
+++ src/components/pages/ChooseAccount/index.js
@@ -4,6 +4,16 @@ import StyledView from '../../atoms/StyledView';
 import StyledText from '../../atoms/StyledText';
 import StyledButton from '../../atoms/StyledButton';
 
+const styles = {
+  container: { flex: 1, justifyContent: 'center' },
+  viewDefaultStyle: {flex:1, flexDirection:'row',justifyContent:"center", alignItems:"center", padding: 20, margin:10, backgroundColor: '#fff', borderColor: '#D3D3D3', borderWidth: 1},
+  viewSelectedStyle: {flex:1, flexDirection:'row',justifyContent:"center", alignItems:"center", padding: 20, margin:10, backgroundColor: '#ccebff', borderColor: '#0000FF',borderWidth: 1},
+  imageStyle: {width: 50, height: 50, flex: 1, marginRight: 30},
+  childViewStyle: {flex:3,flexDirection:'column'},
+  textStyle: { marginBottom:10 },
+  bottomView: { padding: 10},
+  continueButton: { width:"70%"}
+}
 export default class ChooseAccount extends PureComponent {
   state = {
     selectAccount: false,
@@ -24,7 +34,7 @@ export default class ChooseAccount extends PureComponent {
     {
       'imgUrl' :'https://i.imgur.com/K3KJ3w4h.jpg',
       'bankName': 'HSBC Bank (UK)',
-      'accountType':'`Corporate` Account',
+      'accountType':'Corporate Account',
       'sortCode':'20-01-89',
       'accountNumber': '54923767'
     }]
@@ -37,21 +47,19 @@ export default class ChooseAccount extends PureComponent {
   
 
 renderItem = ({item}) => {
-  const bdColor = item.accountNumber === this.state.selectAccount ? 'blue': 'lightgray';
-  const bgColor = item.accountNumber === this.state.selectAccount ? '#ccebff': '#fff';
+  const viewStyle = item.accountNumber === this.state.selectAccount ? styles.viewSelectedStyle: styles.viewDefaultStyle;
     return( 
       <TouchableOpacity onPress={ () => this.selectAccount(item.accountNumber)}>
-        <View style={{flex:1, flexDirection:'row',justifyContent:"center", alignItems:"center", padding: 20, margin:10,borderColor: bdColor, backgroundColor:bgColor, borderStyle:"solid", borderWidth:1
-            }}>
-          <Image source={{ uri: item.imgUrl }} style={{width: 50, height: 50, flex: 1, marginRight: 30}}></Image>
-          <View style={{flex:3,flexDirection:'column'}}>
-            <Text style={{marginBottom:10}}>
-              {item.bankName}
+        <View style={viewStyle}>
+          <Image source={{ uri: item.imgUrl }} style={styles.imageStyle}></Image>
+          <View style={styles.childViewStyle}>
+            <Text style={styles.textStyle}>
+              {item.bankName} 
               </Text>
-              <Text style={{marginBottom:10}}>
+              <Text style={styles.textStyle}>
               {item.accountType}
               </Text>
-              <Text style={{marginBottom:10}}>
+              <Text style={styles.textStyle}>
               {item.sortCode}  {item.accountNumber}
             </Text>
           </View>
@@ -78,8 +86,8 @@ renderItem = ({item}) => {
                 extraData= {this.state}
               /> 
         </StyledView>
-        <StyledView style={{ padding: 10 }}>
-              <StyledButton  disabled={!this.state.selectAccount}  style={{fontSize:12, width:"100%" }}
+        <StyledView style={styles.bottomView}>
+              <StyledButton  disabled={!this.state.selectAccount}  style={styles.continueButton}
                 onPress={() => this.props.navigation.navigate('Dashboard')}>
                 Select & Continue
               </StyledButton>
