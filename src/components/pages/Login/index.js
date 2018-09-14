0402193fd7c7b92e1c56f90903ca649f4eb9db59
Maciek Kurek <maciejk77@gmail.com>
2018-09-14 09:55:49 +0100

    feat(login): updated snaphots

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -1,21 +1,20 @@
 import React, { PureComponent } from 'react';
-import {
-  View, TouchableOpacity, Text,
-} from 'react-native';
+import { View } from 'react-native';
 import StyledTextInput from '../../atoms/StyledTextInput';
 import StyledText from '../../atoms/StyledText';
 import StyledButton from '../../atoms/StyledButton';
+import StyledView from '../../atoms/StyledView';
 
 const styles = {
-  container: { flex: 1, justifyContent: 'center' },
+  container: { justifyContent: 'center' },
   centered: { textAlign: 'center' },
   button: { marginTop: 20 },
-}
+};
 
 export default class Login extends PureComponent {
   render() {
     return (
-      <View style={styles.container}>
+      <StyledView style={styles.container}>
         <View>
           <StyledText weight style={styles.centered}>USERNAME</StyledText>
           <StyledTextInput />
@@ -24,13 +23,13 @@ export default class Login extends PureComponent {
           <StyledText weight style={styles.centered}>PASSWORD</StyledText>
           <StyledTextInput password />
         </View>
-        <StyledButton 
+        <StyledButton
           style={styles.button}
           onPress={() => this.props.navigation.navigate('Setup')}
         >
           LOGIN
         </StyledButton>
-      </View>
+      </StyledView>
     );
   }
 }
