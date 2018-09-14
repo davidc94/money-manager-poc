0c1e4950fb3553b4be19c893b29aa0315a7c2cb2
Maciek Kurek <maciejk77@gmail.com>
2018-09-14 12:27:40 +0100

    feat(donut-chart): component with fixed data added

--- src/components/organisms/SpentSoFar/index.js
+++ src/components/organisms/SpentSoFar/index.js
@@ -1,13 +1,15 @@
 import React, { PureComponent } from 'react';
 import { View, Text } from 'react-native';
+import PieChart from '../PieChart';
+import StyledView from '../../atoms/StyledView';
 
 export default class SpentSoFar extends PureComponent {
-
   render() {
     return (
-      <View>
+      <StyledView>
         <Text>Spent So Far</Text>
-      </View>
+        <PieChart />
+      </StyledView>
     );
   }
 }
