6aaae19dcd2a00ea9c94678ce5443e5c1e0a1892
Tamas Jonas <jonastamas2@gmail.com>
2018-09-17 15:00:30 +0100

    Feat(choose bank page): added choose bank page (#29)
    * feat(choose-bank-page): added choose bank page
    
    * feat(choose-bank-page): add test for flatlist

--- src/components/pages/ChooseBank/index.spec.js
+++ src/components/pages/ChooseBank/index.spec.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import ChooseBank from './index';
+import TestRenderer from 'react-test-renderer';
+import ChooseBank from '.';
 
 configure({ adapter: new Adapter() });
 
 describe('choose bank', () => {
-  const render = shallow(<ChooseBank />);
-
+  const wrapper = shallow(<ChooseBank />);
   it('renders as expected', () => {
-    expect(render).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Selecting first item and navigate to next screen', () => {
+    const navigation = { navigate: jest.fn() };
+    const testrenderer = TestRenderer.create(<ChooseBank navigation={navigation} />);
+    const testinstance = testrenderer.root;
+
+    testinstance.findAllByProps({ 'data-test': 'bank-list-item' })[0].props.onPress();
+    expect(testinstance.findByProps({ 'data-test': 'nextButton' }).props.disabled).toBe(false);
+
+    testinstance.findByProps({ 'data-test': 'nextButton' }).props.onPress();
+    expect(navigation.navigate).toHaveBeenCalledWith('Consent');
   });
 });
