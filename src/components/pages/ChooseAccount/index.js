b1f48336daf026514b76171dba8ab15c21351ff2
sanjeevbaurai <sanjeev.baurai@gmail.com>
2018-09-17 13:16:05 +0100

    feat(account-type):css styles changed

--- src/components/pages/ChooseAccount/index.js
+++ src/components/pages/ChooseAccount/index.js
@@ -6,37 +6,37 @@ import StyledButton from '../../atoms/StyledButton';
 
 const styles = {
   container: { flex: 1, justifyContent: 'center' },
-  viewDefaultStyle: {flex:1, flexDirection:'row',justifyContent:"center", alignItems:"center", padding: 20, margin:10, backgroundColor: '#fff', borderColor: '#D3D3D3', borderWidth: 1},
-  viewSelectedStyle: {flex:1, flexDirection:'row',justifyContent:"center", alignItems:"center", padding: 20, margin:10, backgroundColor: '#ccebff', borderColor: '#0000FF',borderWidth: 1},
-  imageStyle: {width: 50, height: 50, flex: 1, marginRight: 30},
+  viewDefaultStyle: {flex:1, flexDirection:'row',justifyContent:"center", alignItems:"center", padding: 20, margin:10, backgroundColor: '#fff', borderColor: '#D3D3D3', borderWidth: 1, borderRadious: 5},
+  viewSelectedStyle: {flex:1, flexDirection:'row',justifyContent:"center", alignItems:"center", padding: 20, margin:10, backgroundColor: '#ccebff', borderColor: '#0000FF',borderWidth: 1,borderRadious: 5},
+  imageStyle: {width: 50, height: 50, flex: 1, marginRight: 30,resizeMode: 'contain'},
   childViewStyle: {flex:3,flexDirection:'column'},
   textStyle: { marginBottom:10 },
-  bottomView: { padding: 10},
-  continueButton: { width:"70%"}
+  bottomView: { padding: 10, margin:20, alignItems: 'center'},
+  continueButton: { width:"68%"}
 }
 export default class ChooseAccount extends PureComponent {
   state = {
     selectAccount: false,
     data:[{
-      'imgUrl' :'https://i.imgur.com/K3KJ3w4h.jpg',
-      'bankName': 'HSBC Bank (UK)',
-      'accountType':'Advance Account',
-      'sortCode':'20-01-89',
-      'accountNumber': '54923765'
+      logoImage: require('../../../assets/images/banklogos/hsbclogo.jpg'),
+      bankName: 'HSBC Bank (UK)',
+      accountType: 'Advance Account',
+      sortCode: '20-01-89',
+      accountNumber: '54923765'
     },
     {
-      'imgUrl' :'https://i.imgur.com/K3KJ3w4h.jpg',
-      'bankName': 'HSBC Bank (UK)',
-      'accountType':'Premier Account',
-      'sortCode':'20-01-89',
-      'accountNumber': '54923766'
+      logoImage: require('../../../assets/images/banklogos/hsbclogo.jpg'),
+      bankName: 'HSBC Bank (UK)',
+      accountType:'Premier Account',
+      sortCode: '20-01-89',
+      accountNumber: '54923766'
     },
     {
-      'imgUrl' :'https://i.imgur.com/K3KJ3w4h.jpg',
-      'bankName': 'HSBC Bank (UK)',
-      'accountType':'Corporate Account',
-      'sortCode':'20-01-89',
-      'accountNumber': '54923767'
+      logoImage: require('../../../assets/images/banklogos/hsbclogo.jpg'),
+      bankName: 'HSBC Bank (UK)',
+      accountType:'Corporate Account',
+      sortCode: '20-01-89',
+      accountNumber: '54923767'
     }]
   }
 
@@ -51,7 +51,7 @@ renderItem = ({item}) => {
     return( 
       <TouchableOpacity onPress={ () => this.selectAccount(item.accountNumber)}>
         <View style={viewStyle}>
-          <Image source={{ uri: item.imgUrl }} style={styles.imageStyle}></Image>
+          <Image source={ item.logoImage } style={styles.imageStyle}></Image>
           <View style={styles.childViewStyle}>
             <Text style={styles.textStyle}>
               {item.bankName} 
