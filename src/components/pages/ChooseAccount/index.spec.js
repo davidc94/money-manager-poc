3da5941a74a0013fb13c7b0ad4d86f661dd10833
sanjeevbaurai <sanjeev.baurai@gmail.com>
2018-09-17 15:31:09 +0100

    updated in testing for account selection

--- src/components/pages/ChooseAccount/index.spec.js
+++ src/components/pages/ChooseAccount/index.spec.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import TestRenderer from 'react-test-renderer';
 import ChooseAccount from './';
 
 configure({ adapter: new Adapter() });
 
 describe('choose account', () => {
-  const navigation = { navigate: jest.fn() };
-  const render = shallow(<ChooseAccount navigation={navigation} />);
-
+  const wrapper = shallow(<ChooseAccount />);
   it('renders as expected', () => {
-    expect(render).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it('Test select account event', () => {
-    render.find('FlatList').first().simulate('press');
-    render.find('StyledButton').simulate('press');
+  it('Selecting first item and navigate to next screen', () => {
+    const navigation = { navigate: jest.fn() };
+    const testrenderer = TestRenderer.create(<ChooseAccount navigation={navigation} />);
+    const testinstance = testrenderer.root;
+
+    testinstance.findAllByProps({ 'data-test': 'account-list-item' })[0].props.onPress();
+    expect(testinstance.findByProps({ 'data-test': 'nextButton' }).props.disabled).toBe(false);
+
+    testinstance.findByProps({ 'data-test': 'nextButton' }).props.onPress();
     expect(navigation.navigate).toHaveBeenCalledWith('Dashboard');
   });
 });
