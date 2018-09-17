2836fca1dc24b823a95ce3a4df812d58d324ca27
Tamas Jonas <jonast@Tamass-MacBook-Pro.local>
2018-09-17 14:37:49 +0100

    feat(choose-bank-page): add test for flatlist

--- src/components/pages/ChooseBank/index.js
+++ src/components/pages/ChooseBank/index.js
@@ -1,21 +1,40 @@
 import React, { PureComponent } from 'react';
-import { View, TouchableOpacity, Text, Image, FlatList } from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
 import StyledButton from '../../atoms/StyledButton';
 import StyledText from '../../atoms/StyledText';
 
+const styles = StyleSheet.create({
+  bankItem: {
+    flexDirection: 'row',
+    padding: 20,
+    marginTop: 10,
+    marginHorizontal: 10,
+    borderWidth: 1,
+  },
+});
+
 export default class ChooseBank extends PureComponent {
-  state = {
-    data: [{
-      bankName: 'Barclays',
-      logoImage: require('../../../assets/images/banklogos/barclayslogo.jpg')
-    }, {
-      bankName: 'LloydsBank',
-      logoImage: require('../../../assets/images/banklogos/lloydslogo.jpg')
-    }, {
-      bankName: 'HSBC',
-      logoImage: require('../../../assets/images/banklogos/hsbclogo.jpg')
-    }],
-    selectedBank: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [{
+        bankName: 'Barclays',
+        logoImage: require('../../../assets/images/banklogos/barclayslogo.jpg'),
+      }, {
+        bankName: 'LloydsBank',
+        logoImage: require('../../../assets/images/banklogos/lloydslogo.jpg')
+      }, {
+        bankName: 'HSBC',
+        logoImage: require('../../../assets/images/banklogos/hsbclogo.jpg')
+      }],
+      selectedBank: false,
+    }
   }
 
   selectBank = (selectedBank) => this.setState({ selectedBank });
@@ -23,10 +42,13 @@ export default class ChooseBank extends PureComponent {
   renderListItem = ({ item }) => {
     const bdColor = item.bankName === this.state.selectedBank ? '#000' : '#ccc';
     return (
-      <TouchableOpacity onPress={() => this.selectBank(item.bankName)}
-        style={{ flexDirection: 'row', padding: 20, marginTop: 10, marginHorizontal: 10, borderColor: bdColor, borderWidth: 1 }}>
+      <TouchableOpacity
+        onPress={() => this.selectBank(item.bankName)}
+        style={[styles.bankItem, { borderColor: bdColor }]}
+        data-test="bank-list-item"
+      >
         <View style={{ width: 50, height: 50, margin: 5, flex: 1 }}>
-          <Image style={{ resizeMode: 'contain' }} source={item.logoImage} />
+          <Image style={{ width: '100%', resizeMode: 'contain' }} source={item.logoImage} />
         </View>
         <StyledText style={{ flex: 4 }}>
           {item.bankName}
@@ -45,11 +67,13 @@ export default class ChooseBank extends PureComponent {
           data={this.state.data}
           extraData={this.state}
           renderItem={this.renderListItem}
+          keyExtractor={(item) => item.bankName}
         />
         <View style={{ paddingHorizontal: 80 }}>
           <StyledButton
             disabled={!this.state.selectedBank}
             onPress={() => this.props.navigation.navigate('Consent')}
+            data-test="nextButton"
           >
             Connect to bank
           </StyledButton>
