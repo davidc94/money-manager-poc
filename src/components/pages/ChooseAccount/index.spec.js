07a3945c35ca755984ad1a7b765951819e7ca499
Sanjeev Baurai <sanjeev.baurai@gmail.com>
2018-09-17 14:54:36 +0100

    feat(select-bank-account-page): added select account page (#19)
    * feat(select-bank-account-page): add functionality
    
    * feat(select-account): remove inline styling
    
    * feat(account-type):css styles changed
    
    * feat(account-type):updatd minor changes in css

--- src/components/pages/ChooseAccount/index.spec.js
+++ src/components/pages/ChooseAccount/index.spec.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import ChooseAccount from './index';
+import ChooseAccount from './';
 
 configure({ adapter: new Adapter() });
 
 describe('choose account', () => {
-  const render = shallow(<ChooseAccount />);
+  const navigation = { navigate: jest.fn() };
+  const render = shallow(<ChooseAccount navigation={navigation} />);
 
   it('renders as expected', () => {
     expect(render).toMatchSnapshot();
+  });
+
+  it('Test select account event', () => {
+    render.find('FlatList').first().simulate('press');
+    render.find('StyledButton').simulate('press');
+    expect(navigation.navigate).toHaveBeenCalledWith('Dashboard');
   });
 });
