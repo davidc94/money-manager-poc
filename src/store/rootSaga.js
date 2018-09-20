35c82c8d5d6257742f21737e86a7f91032ee1577
Tamas Jonas <jonastamas2@gmail.com>
2018-09-20 09:46:33 +0100

    feat(bank-balance):added bb page (#39)

--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -2,11 +2,13 @@ import { all } from 'redux-saga/effects';
 import fetchDataWatcher from './banks/sagas';
 import fetchBankBalanceWatcher from './dashboard/bankBalance/sagas';
 import fetchIncomeWatcher from './dashboard/income/sagas';
+import fetchBankBalanceWatcher from './dashboard/bankBalance/sagas';
 
 export default function* rootSagas() {
   yield all([
     fetchDataWatcher(),
     fetchBankBalanceWatcher(),
     fetchIncomeWatcher(),
+    fetchBankBalanceWatcher(),
   ]);
 }
