9bd01fe5ac50c558261faa4029657ea64fc0492e
Maciek Kurek <maciejk77@gmail.com>
2018-09-21 12:04:58 +0100

    fix(setup-text): styling corrected

--- src/components/pages/Setup/index.js
+++ src/components/pages/Setup/index.js
@@ -4,35 +4,37 @@ import StyledView from '../../atoms/StyledView';
 import StyledText from '../../atoms/StyledText';
 import StyledButton from '../../atoms/StyledButton';
 
-const styles = {
-  container: { flex: 1, justifyContent: 'center' },
-}
-
 export default class Setup extends PureComponent {
   render() {
+
+    const { header, step, text, button } = styles;
+
     return (
       <ScrollView>
-        <StyledView style={styles.container}>
-          <StyledText size weight>How does this set up work?</StyledText>
-          <StyledText weight>Step 1</StyledText>
-          <StyledText>
+        <StyledView style={header}>
+          <StyledText size weight>How does this</StyledText>
+          <StyledText size weight>set up work?</StyledText>
+          <StyledText weight style={step}>Step 1</StyledText>
+          <StyledText style={text}>
             Choose your bank -  You select your bank from the list of
             selected bank we work with!
           </StyledText>
-          <StyledText weight>Step 2</StyledText>
-          <StyledText>
+          
+          <StyledText weight style={step}>Step 2</StyledText>
+          <StyledText style={text}>
             Your consent - We need your permission so
             that we can connect to your selected bank
             and one of the current account.
           </StyledText>
-          <StyledText weight>Step 3</StyledText>
-          <StyledText>
+          <StyledText weight style={step}>Step 3</StyledText>
+          <StyledText style={text}>
             Choose bank account - You will choose the bank account
             that you use for your day to day spend (if you have more
             than one current account)
           </StyledText>
           <StyledButton 
             className="button"
+            style={button}
             onPress={() => this.props.navigation.navigate('ChooseBank')}
           >
             Continue
@@ -42,3 +44,23 @@ export default class Setup extends PureComponent {
     );
   }
 }
+
+const styles = {
+  header: {
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  step: {
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  text: {
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  button: {
+    marginTop: 25,
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+};
