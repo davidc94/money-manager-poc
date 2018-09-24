fda820341a795837d813e59de538fd4972e828ed
sanjeevbaurai <sanjeev.baurai@gmail.com>
2018-09-24 12:12:26 +0100

    feat(login): add functionality and testing

--- src/components/atoms/StyledTextInput/index.js
+++ src/components/atoms/StyledTextInput/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import PropTypes from 'prop-types';
 
 const TextInputStyling = styled.TextInput`
   border-color: gray;
   border-width: 2px;
   width: 95%;
+  height: 40
   margin: 0 auto;
-  paddingLeft: 20;
+  paddingLeft: 45;
   fontSize: 20;
+  marginHorizontal: 20;
+  backgroundColor: 'rgba(255, 255, 255, 0.4)';
+  borderRadius: 20;
 `;
 
 const StyledTextInput = props => (
   <TextInputStyling
-    secureTextEntry={!!props.password}
+  // !!props.password
+    secureTextEntry={props.secureTextEntry}
     underlineColorAndroid="rgba(0,0,0,0)"
+    autoCapitalize={props.autoCapitalize}
+    autoCorrect={props.autoCorrect}
+    placeholder={props.placeholder}
+    returnKeyType={props.returnKeyType}
+    onChangeText={ props.onChangeText }
   />
 );
 
