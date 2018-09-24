7c3c1d340f37eb50543c54730f8ba2efec810b34
Tamas Jonas <jonast@Tamass-MacBook-Pro.local>
2018-09-24 12:05:03 +0100

    feat(styling-bank-balance): added styles to bank balance

--- src/components/organisms/BankBalance/index.js
+++ src/components/organisms/BankBalance/index.js
@@ -1,10 +1,47 @@
 import React, { PureComponent } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 import { connect } from 'react-redux';
 
 import { FETCH_BANK_BALANCE } from '../../../store/constants';
 import bankImages from '../../../assets/images/banklogos/index';
 import StyledText from '../../atoms/StyledText';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'flex-start',
+  },
+  headerContainer: {
+    paddingBottom: 20,
+    marginVertical: 20,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  bankDetailsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bankName: { fontSize: 24 },
+  balanceContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    marginVertical: 20,
+    backgroundColor: '#ccc',
+  },
+  balanceTitle: { fontSize: 28 },
+  balanceAmount: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  }
+});
 
 class BankBalance extends PureComponent {
 
@@ -17,35 +54,23 @@ class BankBalance extends PureComponent {
       return null;
     }
     const {
-      bankBalance: { balance, userName },
-      selectedBank: { bankName, logoImage }
+      bankBalance: { balance },
+      selectedBank: { bankName }
     } = this.props;
     return (
-      <View style={{ flex: 1, padding: 20, justifyContent: 'flex-start' }}>
-        <View style={{ paddingBottom: 20, marginVertical: 20, borderBottomColor: '#ccc', borderBottomWidth: 1 }}>
-          <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Bank balance</Text>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>Bank balance</Text>
         </View>
-        <View style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}>
-          <Text style={{ fontSize: 24 }}>{bankName}</Text>
+        <View style={styles.bankDetailsContainer}>
+          <Text style={styles.bankName}>{bankName}</Text>
           <Image source={bankImages[bankName]} />
         </View>
-        <View style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          paddingHorizontal: 20,
-          paddingVertical: 30,
-          marginVertical: 20,
-          backgroundColor: '#ccc',
-        }}>
-          <Text style={{ fontSize: 28 }}>Balance</Text>
-          <Text style={{ fontSize: 28, fontWeight: 'bold' }}>£{balance}</Text>
+        <View style={styles.balanceContainer}>
+          <Text style={styles.balanceTitle}>Balance</Text>
+          <Text style={styles.balanceAmount}>{`£${balance}`}</Text>
         </View>
       </View>
-
     );
   }
 }
