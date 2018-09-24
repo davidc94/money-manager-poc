3d20699b06521fb8e723ab9c5f3274c7c8de7fa4
Sanjeev Baurai <sanjeev.baurai@gmail.com>
2018-09-24 12:16:10 +0100

    feat(login): add functionality and testing (#48)

--- src/components/pages/Login/index.spec.js
+++ src/components/pages/Login/index.spec.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
+import configureStore from 'redux-mock-store';
 import Adapter from 'enzyme-adapter-react-16';
 import Login from './index';
 
 configure({ adapter: new Adapter() });
 
 describe('login', () => {
-  const render = shallow(<Login />);
+
+  const initialState = {
+    login: {
+      userList: [
+        {
+          user: 'user1',
+          userName: 'username1',
+          emailID: 'email1',
+          password: 'password1',
+        },
+        {
+          user: 'user2',
+          userName: 'username2',
+          emailID: 'email2',
+          password: 'password2',
+        },
+      ],
+    },
+  };
+
+  const mockStore = configureStore();
+  const store = mockStore(initialState);
+  const render = shallow(<Login store={store} />);
 
   it('renders as expected', () => {
     expect(render).toMatchSnapshot();
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import bank from './banks/reducer';
 import bankBalance from './dashboard/bankBalance/reducer';
 import income from './dashboard/income/reducer';
+import login from './login/reducer';
 
 const rootReducer = combineReducers({
   bank,
   bankBalance,
   income,
+  login,
 });
 
 export default rootReducer;
