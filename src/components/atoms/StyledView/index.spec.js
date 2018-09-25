5a6ca336682cb2bae5abb5d3d3d36815b230b518
Maciek Kurek <maciejk77@gmail.com>
2018-09-25 16:35:39 +0100

    fix(test-imports): tests fixed

--- src/components/atoms/StyledView/index.spec.js
+++ src/components/atoms/StyledView/index.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import StyledView from './index';
+import { StyledView } from './index';
 
 configure({ adapter: new Adapter() });
 
