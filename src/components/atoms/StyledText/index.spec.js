2995e0078c83a9a4b33f27950ad67ae25a8e5af6
Maciek Kurek <maciejk77@gmail.com>
2018-09-25 16:37:52 +0100

    fix(test-imports): tests fixed
    fix(test-imports): tests fixed

--- src/components/atoms/StyledText/index.spec.js
+++ src/components/atoms/StyledText/index.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import StyledText from './index';
+import { StyledText } from './index';
 
 configure({ adapter: new Adapter() });
 
