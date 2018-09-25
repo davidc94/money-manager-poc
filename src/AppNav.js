9b387b2c674ea2c9ca3e12ace405f41c2d68a7a1
Sanjeev Baurai <sanjeev.baurai@gmail.com>
2018-09-25 12:29:42 +0100

    feat(login-page): added keyboardavoidview and remove logo
    feat(login-page): added keyboard avoid view and remove logo

--- src/AppNav.js
+++ src/AppNav.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
 import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import Login from './components/pages/Login';
 import Setup from './components/pages/Setup';
@@ -5,6 +7,7 @@ import ChooseBank from './components/pages/ChooseBank';
 import Consent from './components/pages/Consent';
 import ChooseAccount from './components/pages/ChooseAccount';
 import Dashboard from './components/pages/Dashboard';
+import headerImg from './assets/images/userlogin/headerImg.png';
 
 export const switchNav = createSwitchNavigator(
   {
@@ -26,14 +29,37 @@ export default createStackNavigator(
   },
   {
     navigationOptions: {
-      title: 'gameplan Money Manager',
-      headerTintColor: '#ffffff',
-      barStyle: 'light-content',
-      headerStyle: {
-        backgroundColor: '#000000',
-        height: 25,
-      },
+     //console.log(navigation)
+    //   if (navigation.state.routeName !== 'Login'){
+    //     return(
+        headerStyle: {
+            backgroundColor: '#000000',
+            height: 28,
+        },
+        headerTitle: (
+          <View style={{flex: 1,
+            backgroundColor:'#000', 
+            justifyContent: 'center',
+            alignItems:'flex-start',
+            paddingLeft:5,}}> 
+            <Image  style={{width: '45%', height: 45}} source={ headerImg }/>
+          </View>
+        ),
+    //   )
+    // }   
     },
-    cardStyle: { shadowColor: 'transparent' },
-  },
-);
+  
+  cardStyle: { shadowColor: 'transparent' },
+});
+
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor:'#000', 
+//     justifyContent: 'center',
+//     alignItems:'flex-start',
+//     paddingLeft:5,
+//   },
+// });
